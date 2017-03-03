bdb01c4254dacdefead0afa36135ff961ecec873
nisk782 <nskaalum@gmail.com>
2017-03-03 16:17:44 -0400

    filled insinc functions - still untested. No logging yet.

--- insinc/app.js
+++ insinc/app.js
@@ -3,29 +3,101 @@ var app = express();
 var bodyParser = require("body-parser");
 var assert = require("assert");
 var mongo = require("mongodb").MongoClient;
-var url = "mongodb://localhost:27017/4145p1";
+var restClient = require("node-rest-client").Client;
+var dbUrl = "mongodb://localhost:27017/4145p1";
+var logUrl = "";  //logger method url
+var mbrUrl = "/mbr/submit_insurance_quote"; //complete this
+var reCollection, munCollection;
 app.use(bodyParser.json());
 
+//accept info from RE
 app.post("/realestate", function(req, res){
   //log
-  //store mortId, mlsId, appraiseValue in db
-  //check for municipal, submitQuote if so
+
+  //add realestate info to db
+  reCollection.find({
+    mortID : req.body.mortID
+  }).toArray(function(err, result){
+    assert.equal(err, null);
+    if(result.length > 0){
+      reCollection.insert({
+        mortID : req.body.mortID,
+        mlsID : req.body.mlsID,
+        appraiseValue : req.body.appraiseValue
+      });
+    }
+  });
+
+  //submit quote if municipal info exists
+  munCollection.find({
+    mortID : req.body.mortID
+  }).toArray(function(err, result){
+    assert.equal(err, null);
+    if(result.length > 0){
+      submitQuote(req.body.mlsID);
+    }
+  });
+
+  res.sendStatus(200);
 });
 
-app.post("municipal", function(req, res){
+//accept info from MUN
+app.post("/municipal", function(req, res){
   //log
-  //store mortId, mlsId, array in db
-  //check for realestate, submitQuote if so
+
+  //add municipal info to db
+  munCollection.find({
+    mortID : req.body.mortID
+  }).toArray(function(err, result){
+    assert.equal(err, null);
+    if(result.length > 0){
+      reCollection.insert({
+        mortID : req.body.mortID,
+        mlsID : req.body.mlsID,
+        services : req.body.services
+      });
+    }
+  });
+
+  //submit quote if realestate info exists
+  reCollection.find({
+    mortID : req.body.mortID
+  }).toArray(function(err, result){
+    assert.equal(err, null);
+    if(result.length > 0){
+      submitQuote(req.body.mlsID);
+    }
+  });
+
+  res.sendStatus(200);
 });
 
-function submitQuote(mortId){
-  //send mlsId, insuranceValue, deductableValue, clientName to MBR
+//sends quote to MBR
+function submitQuote(mlsID){
+  var client = new restClient();
+  var payload = {
+    mlsID : mlsID,
+    insured_value : 0,
+    deductable_value : 0,
+    name : "bob"
+  };
+  var args = {
+    headers: {"content-type": "application/json"},
+    data: JSON.stringify(payload)
+  };
+
+  client.post(mbrUrl, args);
 }
 
-mongo.connect(url, function(err, db){
+function log(){
+  //send stuff to logUrl
+}
+
+mongo.connect(dbUrl, function(err, db){
   assert.equal(null, err);
   console.log("Connected to server");
-  collection = db.collection("documents");
+  reCollection = db.collection("realestate");
+  munCollection = db.collection("municipal");
 });
 
 app.listen(53045, function(){
