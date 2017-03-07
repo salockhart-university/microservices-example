90feaa26c9c797f24b869b8a3990391407e0b861
Nick Broderick <nsbroderick@outlook.com>
2017-03-06 21:23:10 -0400

    Getting everything to a sane place in emp: it compiles but does nothing

--- emp/app.js
+++ emp/app.js
@@ -1,14 +1,15 @@
 (function () {
   'use strict';
 
-  const production = process.env.NODE_ENV === 'production'
+  const production = process.env.NODE_ENV === 'production';
   const config = production ? require('../config/production.json').hostnames.emp
                             : require('../config/local.json').hostnames.emp;
 
   const express = require('express');
   const path = require('path');
   const http = require('http');
-  const dbConn = require('./dbConn.js');
+  // const dbConn = require('./dbConn.js');
+  const dbConn = {};
 
   const app = express();
 
@@ -16,6 +17,7 @@
   setAppSecret(app);
   injectAuthoirzationTokenParser(app);
   setViewPath(app);
+  loadRoutes(app, dbConn);
 
   http.createServer(app).listen(config.port);
 
@@ -24,7 +26,7 @@
     const cookieParser = require('cookie-parser');
 
     app.use(bodyParser.json());
-    app.use(bodyParser.urlEncoded({ extended: true }));
+    app.use(bodyParser.urlencoded({ extended: true }));
     app.use(cookieParser());
   }
 
@@ -39,7 +41,7 @@
     const jwt = require('jsonwebtoken');
 
     app.use(function (req, res, next) {
-      const tokenName = 'csci4145_emp_access_token';
+      const tokenName = 'CSCI4145_EMP_ACCESS_TOKEN';
       const token = req.cookies[tokenName];
       if (token) {
         jwt.verify(token, app.get('secret'), function (err, decoded) {
@@ -62,4 +64,44 @@
     app.set('views', viewPath);
   }
 
+  function loadRoutes(app, dbConn) {
+    const fs = require('fs');
+    const routes = path.join(__dirname, 'routes');
+    const secureRoutes = path.join(__dirname, 'secure-routes');
+
+    fs.readdirSync(routes)
+      .map(file => path.join(routes, file))
+      .forEach(loadRoute);
+
+    injectAuthorizationBarrier(app);
+
+    fs.readdirSync(secureRoutes)
+      .map(file => path.join(secureRoutes, file))
+      .forEach(loadRoute);
+
+    function loadRoute(path) {
+      let ext = path.extname(path);
+      if (ext === '.js') {
+        const routeInit = require(path);
+        if (typeof routeInit === 'function') {
+          routeInit(app, dbConn);
+        }
+        else {
+          console.log(`Error loading route '${path}`);
+          process.exit(1);
+        }
+      }
+    }
+
+    function injectAuthorizationBarrier(app) {
+      app.use(function (req, res, next) {
+        if (req.signedInUser) {
+          next();
+        }
+        else {
+          res.render('access-denied');
+        }
+      });
+    }
+  }
 })();
