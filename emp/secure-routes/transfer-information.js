db4247686f6ca4fb6ecdc419145921e2e7a698e5
Nick Broderick <nsbroderick@outlook.com>
2017-03-10 14:03:01 -0400

    Implemented endpoint to transfer information to MBR

--- emp/secure-routes/transfer-information.js
+++ emp/secure-routes/transfer-information.js
@@ -1,7 +1,13 @@
 (function () {
   'use strict';
 
-  module.exports = function createHomeRoute(app, dbConn) {
+  const makeRequest = require('../../common/request.js').makeRequest;
+  const production = process.env.NODE_ENV === 'production';
+  const config = production ?
+      require('../../config/production.json')
+    : require('../../config/local.json');
+
+  module.exports = function createTransferInformationRoute(app) {
     app.get('/transfer-information', function (req, res) {
       res.render('transfer-information', {
         action: '/transfer-information',
@@ -12,13 +18,29 @@
     });
 
     app.post('/transfer-information', function (req, res) {
-      // Temporary thing until I figure out how to contact EMP
-      res.render('transfer-information', {
-        action: '/transfer-information',
-        form: transferInformationForm,
-        buttonText: 'Send information to MBR',
-        signedInUser: req.signedInUser
-      });
+      const endpoint = '/mbr/submit_employer_info';
+      const { domain, port } = config.hostnames.mbr;
+      const body = makeRequestBody(req);
+
+      makeRequest(domain, port, endpoint, 'POST', body)
+        .then(function () {
+          res.render('transfer-information-result');
+        })
+        .catch(function (error) {
+          res.render('transfer-information-result', { error });
+        });
+
+      function makeRequestBody(req) {
+        const firstName = req.signedInUser.firstName;
+        const lastName = req.signedInUser.lastName;
+
+        const mortID = req.body.mortId;
+        const name = [ firstName, lastName ].join(' ');
+        const salary = req.signedInUser.salary;
+        const employment_start = req.signedInUser.startDate;
+
+        return { mortID, name, salary, employment_start };
+      }
     });
   };
 
@@ -28,6 +50,6 @@
       label: 'mortID',
       required: true,
       name: 'mortId'
-    },
+    }
   ];
 })();
