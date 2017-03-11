448986cf143b50d3989bc2a12629bff73ebb2bee
Alex Lockhart <salexlockhart@gmail.com>
2017-03-11 19:11:11 -0400

    revert log changes

--- common/common.js
+++ common/common.js
@@ -1,66 +1,61 @@
-'use strict';
-
 var http = require('http');
-
-const request = require('./request');
 
 let config;
 let port;
 
 if (process.env.NODE_ENV === "local") {
-	config = require('../config/local.json');
-	port = config.hostnames.log.port;
+  config = require('../config/local.json');
+  port = config.hostnames.log.port;
 } else {
-	config = require('../config/prod.json');
-	port = 80;
+  config = require('../config/prod.json');
+  port = 80;
 }
 
 module.exports = {
-	logInfo: function(service, endpoint, request, response_status, response_body) {
-		var body = {
-			service: service,
-			endpoint: endpoint,
-			request_body: request.body,
-			user_agent: request.headers['user-agent'],
-			response_code: response_status,
-			response_body: response_body
-		}
+  logInfo: function(service, endpoint, request, response_status, response_body) {
+    var body = {
+      service: service,
+      endpoint: endpoint,
+      request_body: request.body,
+      user_agent: request.headers['user-agent'],
+      response_code: response_status,
+      response_body: response_body
+    }
 
-		var options = {
-			host: config.hostnames.log.domain,
-			port: port,
-			path: '/logger/log',
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-				'Content-Length': Buffer.byteLength(JSON.stringify(body))
-			}
-		}
+    var options = {
+      host: config.hostnames.log.domain,
+      port: port,
+      path: '/logger/log',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+  			'Content-Length': Buffer.byteLength(JSON.stringify(body))
+      }
+    }
 
-		return new Promise(function(resolve, reject) {
-			const request = http.request(options, function(result) {
-				result.setEncoding('utf8');
-				const body = [];
-				result.on('data', function(chunk) {
-					body.push(chunk);
-				});
-				result.on('end', function() {
-					const result = body.length === 0 ? '' : Buffer.concat(body).toString();
-					if (result.statusCode >= 400) {
-						return reject(result);
-					}
-					try {
-						resolve(JSON.parse(result));
-					} catch (err) {
-						resolve(result);
-					}
-				});
-			});
-			request.on('error', function(error) {
-				reject(error);
-			});
-			request.write(JSON.stringify(body));
-			request.end();
-		});
-	}
+    return new Promise(function(resolve, reject) {
+      var request = http.request(options, function(result) {
+        if (result.statusCode < 200 || result.statusCode >= 300) {
+          return reject('statusCode=' + result.statusCode);
+        }
+        var body = [];
+        result.on('data', function(chunk) {
+          body.push(chunk);
+        });
+        result.on('end', function() {
+          try {
+            body = JSON.parse(Buffer.concat(body).toString());
+          } catch(e) {
+            reject(e);
+          }
+            resolve(body);
+        });
+      });
+      request.on('error', function(error) {
+        reject(error);
+      });
+      request.write(JSON.stringify(body));
+      request.end();
+    });
+  }
 };
